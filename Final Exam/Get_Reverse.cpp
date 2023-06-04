3c5280b4a12523e6343e02b4fcd0434e08547af7
Md Sakib al hasan <127658885+Md-sakib-al-hasan@users.noreply.github.com>
2023-06-04 08:45:10 +0600

    Add files via upload

---
+++ Final Exam/Get_Reverse.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student {
+    public:
+    string name;
+    int cls;
+    char section;
+    int math_marks;
+    int eng_marks;
+    Student ()
+
+}
+int main()
+{
+     
+     return 0;
+}
